18e5ecc1636bbd6ee59bb309453f928caa522ee3
olaneat <olaneat@yahoo.com>
2018-11-16 11:48:12 +0100

    modification

--- askschools/models.py
+++ askschools/models.py
@@ -6,19 +6,24 @@ from . constant import level, sex, clubs, sport, school_fees, state, school_type
 
 class Schools(models.Model):
 	
-	SCHOOL_NAME = models.CharField(max_length = 300, null = False )
-	MOTTO = models.CharField(null = False, max_length = 200,  )
-	BADGE = models.ImageField(upload_to = "media/images", null = False, blank= True, help_text = "upload a jpg file ")
-	LEVEL = models.CharField(null = False, blank=True, max_length = 20, choices =level )
-	ADVANTAGE = models.TextField(null = True, max_length = 1000, help_text = 'what do parents tend to benefit  by entrusting their children in your school not more than 1000 characters'  )
-	ADDRESS = models.CharField(null = False, max_length  = 250 )
-	TOWN = models.CharField(null = False, max_length = 100, help_text = 'enter the Local Government Area')
-	STATE = models.CharField(null = False, max_length = 4, choices = state)	
-	VIDEO = models.FileField(upload_to = 'media/video', null = False, blank= True, help_text = "upload a video file, mp4, " )	
-	SCHOOL_TYPE = models.CharField(blank = True, max_length = 20, choices = school_type)
-	FEES_RANGE = models.CharField(max_length = 70,  null = True, choices = school_fees  )
+	SCHOOL_NAME = models.CharField(max_length = 300,  )
+	MOTTO = models.CharField( max_length = 200,  )
+	BADGE = models.ImageField(upload_to = "media/images", blank= True,\
+	 help_text = "upload a jpg file ")
+	LEVEL = models.CharField( blank=True, max_length = 20, choices =level )
+	ADVANTAGE = models.TextField(blank = True, max_length = 1000,\
+	 help_text = '''what do parents tend to benefit  by entrusting their children
+	  in your school not more than 1000 characters'''  )
+	ADDRESS = models.CharField( max_length  = 250 )
+	TOWN = models.CharField( max_length = 100,\
+	 help_text = 'enter the Local Government Area')
+	STATE = models.CharField( max_length = 4, choices = state)	
+	VIDEO = models.FileField(upload_to = 'media/video', blank= True,\
+	 help_text = "upload a video file, mp4, " )	
+	SCHOOL_TYPE = models.CharField( max_length = 20, choices = school_type)
+	FEES_RANGE = models.CharField(max_length = 70,  choices = school_fees  )
 	EMAIL = models.EmailField(blank = True, max_length = 50, )
-	PHONE = models.CharField(null = False, max_length = 15)
+	PHONE = models.CharField(blank = True, max_length = 15)
 
 
 
@@ -32,8 +37,10 @@ class school_data(models.Model):
 	CURRICULUM = models.CharField(max_length =20,  choices = curriculum )
 	WEBSITE = models.URLField(max_length = 100, blank = True )
 	EXTRA_CURRICULUM = models.CharField(max_length = 20)
-	AWARDS = models.CharField( max_length = 150, blank = True,  help_text ='kindly list the schools Awards')
-	DIRECTION = models.CharField(max_length = 100, help_text ='give a brief description to your school ' )
+	AWARDS = models.CharField( max_length = 150, blank = True,\
+	  help_text ='kindly list the schools Awards')
+	DIRECTION = models.CharField(max_length = 100,\
+	 help_text ='give a brief description to your school ' )
 	
 	def get_absolute_url(self):
 		return reverse('schools:detail', kwargs={'pk': self.pk})
@@ -42,7 +49,8 @@ class school_data(models.Model):
 
 
 class parents_remark(models.Model):
-	full_name = models.CharField(max_length = 300, help_text = "fill in your full name" )
+	full_name = models.CharField(max_length = 300,\
+	 help_text = "fill in your full name" )
 	school_name= models.CharField(max_length =100 , null = False  )
 	comment = models.TextField(max_length =1000 )
 	
